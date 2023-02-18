25a76c7778d3272737ed193ac30a44acf7532f58
igorbavand <igor.bvn@gmail.com>
2023-02-18 16:37:51 -0300

    Configura cors

--- src/main/java/com/caixa/caixa/modulos/produto/controller/ProdutoController.java
+++ src/main/java/com/caixa/caixa/modulos/produto/controller/ProdutoController.java
@@ -14,6 +14,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.util.UUID;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("api/produtos")
 public class ProdutoController {
 
